c6d091ccc015c23d41a9fb3ce42fc11319748abc
JJ <johnjoe.merritt@outlook.com>
2023-05-10 21:11:05 +0100

    yes now

---
+++ Player-Investigator-UI/Core/ComboBoxViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_Investigator_UI.Core
+{
+    public class ComboBoxViewModel
+    {
+        public List<string> GameCollection { get; set; }
+
+        public ComboBoxViewModel()
+        {
+            GameCollection = new List<string>()
+            {
+                "CS:GO",
+                "DOTA 2",
+                "Apex Legends",
+                
+
+            };
+
+        }
+    }
+}
